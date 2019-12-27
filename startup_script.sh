03446a55a3f434494f0775baddf697570ccec0c0
Konstantin Kondratiuk <kondratukkn@gmail.com>
2019-12-27 23:43:49 +0200

    Fix for Travis tests-3

--- startup_script.sh
+++ startup_script.sh
@@ -15,9 +15,8 @@ sudo apt install -y ruby-full ruby-bundler build-essential
 #echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
 #Fix for Travis tests
-echo 'deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 #Update repositories. This allows apt to read from the newly added MongoDB repo
 sudo apt-get update
